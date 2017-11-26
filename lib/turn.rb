3a2597baa88831893166a0468c34e35f94556c51
Razelle McCarrick <razelle@satyaorthopedic.com>
2017-11-26 22:53:56 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,10 @@
-
+#Define Empty Board with 9 Spaces
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+
+#Displays the Board in the Tic Tac Toe Grid Format Like So:
+#  |  |
+#--------
+#  |  |
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -8,14 +13,17 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+#Take the Number the User Supplies and Subtracts One to == Corresponding Index
 def input_to_index(userinput)
   userinput.to_i - 1
 end
 
-def move(array, index, character = "X")
-  array[index] = character
+#Changes Empty Space on Board to X or O, Default Being X
+def move(board, index, character = "X")
+  board[index] = character
 end
 
+#Checks if Position is Empty Space or Has an X/O Already
 def position_taken?(board, index)
    if board[index] == " " || board[index] == "" || board[index] == nil
      false
@@ -24,6 +32,7 @@ def position_taken?(board, index)
  end
 end
 
+#Checks if Move(Input) is Valid Based on If Position is Taken OR Number is in Correct Range. (9 = index of 8)
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board,index)
 end
